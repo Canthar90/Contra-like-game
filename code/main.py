b535fbf95d6208a1f24c5f3ece44c5bc7d469cdc
Canthar90 <d.cieslak90@gmail.com>
2023-02-23 14:05:53 +0100

    Basic import of assets form tiled map

--- code/main.py
+++ code/main.py
@@ -1,12 +1,25 @@
 import pygame, sys
 from settings import * 
+from pytmx.util_pygame import load_pygame
+from tile import Tile
+
 
 class Main:
 	def __init__(self):
 		pygame.init()
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-		pygame.display.set_caption('Contra')
+		pygame.display.set_caption('Contra_like')
 		self.clock = pygame.time.Clock()
+		
+		# groups
+		self.all_sprites = pygame.sprite.Group()
+		self.setup()
+  
+	def setup(self):
+		tmx_map = load_pygame("data\map.tmx")
+		for x, y, surf in tmx_map.get_layer_by_name("Level").tiles():
+			Tile(pos=(x*64, y*64), surf=surf, groups=self.all_sprites)
+		
 
 	def run(self):
 		while True:
@@ -14,9 +27,12 @@ class Main:
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
-
+		
 			dt = self.clock.tick() / 1000
 			self.display_surface.fill((249,131,103))
+
+			self.all_sprites.update(dt)
+			self.all_sprites.draw(self.display_surface)
 
 			pygame.display.update()
 
